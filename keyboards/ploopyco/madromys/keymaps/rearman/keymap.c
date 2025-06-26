e7e8337694add9e9c49453a0c9c20399fe3ae57e
rearman <rearman@r717.net>
2025-06-26 09:39:36 -0400

    Tweaks to Adept

--- keyboards/ploopyco/madromys/keymaps/rearman/keymap.c
+++ keyboards/ploopyco/madromys/keymaps/rearman/keymap.c
@@ -20,46 +20,24 @@
 extern bool is_drag_scroll;
 uint16_t overall_scroll_distance = 0;
 
-enum {
-	TD_M45,
-};
-
-/* TAP-DANCES */
-tap_dance_action_t tap_dance_actions[] = {
-	[TD_M45] = ACTION_TAP_DANCE_DOUBLE(KC_BTN4, KC_BTN5),
-};
-
-enum rearman_keycodes {
-	M_45 = DRAG_SCROLL + 1,
-};
-
-#define M_45 TD(TD_M45)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT( DRAG_SCROLL, LCA(KC_TAB), M_45, KC_BTN3, KC_BTN2, KC_BTN1 )
+	[0] = LAYOUT( DRAG_SCROLL, LCA(KC_TAB), KC_BTN4, KC_BTN3, KC_BTN2, KC_BTN1 )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	/* Stop scrolling if MB1/2/3 pressed */
-	if (is_drag_scroll && (keycode == KC_BTN1 || keycode == KC_BTN2 || keycode == KC_BTN3)) {
+	if (is_drag_scroll && ((keycode == KC_BTN1 || keycode == KC_BTN2 || keycode == KC_BTN3) ||
+			       (keycode == DRAG_SCROLL && !record->event.pressed &&
+				overall_scroll_distance > REARMAN_PLOOPY_RELEASE_ADHOC_DISTANCE))) {
 		is_drag_scroll = false;
 		pointing_device_set_cpi(dpi_array[keyboard_config.dpi_config]);
 		return false;
 	}
-
-	if (is_drag_scroll && keycode == DRAG_SCROLL && !record->event.pressed && overall_scroll_distance > REARMAN_PLOOPY_RELEASE_ADHOC_DISTANCE) {
-		/* Scroll further than setpoint = ad-hoc scroll */
-		is_drag_scroll = false;
-		pointing_device_set_cpi(dpi_array[keyboard_config.dpi_config]);
-		return false;
-	}
-
 	return true;
 }
 
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
 	if(is_drag_scroll) {
-		overall_scroll_distance += abs(moust_report.v) + abs(mouse_report.h);
+		overall_scroll_distance += abs(mouse_report.v) + abs(mouse_report.h);
 	} else {
 		overall_scroll_distance = 0;
 	}
